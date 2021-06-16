11367fe2d11591af2ad85f17c692af59e1b557f2
Vinicios Wentz <vinicios@wentz.io>
2021-06-15 22:53:29 -0300

    Resolvendo exercícios sobre Spread Operator e Parâmetro Rest

---
+++ ExModulo2/exParametroRest.js
@@ -0,0 +1,5 @@
+function maiorNumero (...parametros) {
+    return Math.min(...parametros)
+};
+
+console.log(maiorNumero(2, 4, 5, 6));
---
+++ ExModulo2/exSpread.js
@@ -0,0 +1,5 @@
+numerosPrimos =[2, 3, 5, 7, 11, 13, 17];
+
+numeros = [...numerosPrimos, 1, 4, 6, 8,9]
+
+console.log(numeros)
